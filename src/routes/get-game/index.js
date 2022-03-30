f05270fc3c4fb9cf73d14d47e3e06d9f2a826d41
Atreya Choudhury <atreyachoudhury@hotmail.com>
2022-03-31 03:48:13 +0530

    alerts

--- src/routes/get-game/index.js
+++ src/routes/get-game/index.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react"
-import { Container, Input, Select, Button, Box } from "@chakra-ui/react"
+import { Container, Input, Select, Button, Box, Alert, AlertIcon } from "@chakra-ui/react"
 import axios from "../../axios"
 
 export default function GetGame() {
 	const [roll, setRoll] = useState("")
 	const [game, setGame] = useState("")
 	const [games, setGames] = useState(null)
+	const [alerts, setAlerts] = useState([])
 
 	useEffect(() => {
 		axios
@@ -20,17 +21,37 @@ export default function GetGame() {
 			event.preventDefault()
 			let data = await axios.post(`/rent/${game}`, { roll: roll })
 			data = data.data
-			console.log(data)
-			setRoll("")
+			const id = games.findIndex((game) => game.id === data.game)
 			setGame("")
+			setRoll("")
+			setGames((games) => {
+				let newGames = Array.from(games)
+				newGames[id] = { ...newGames[id], available: false }
+				return newGames
+			})
+			setAlerts((alerts) => [
+				<Alert mb={1} status="success" borderRadius="md">
+					<AlertIcon />
+					{games[id].name} is yours!
+				</Alert>,
+				...alerts
+			])
 		} catch (err) {
 			console.log(err)
 		}
 	}
 
 	return (
-		<Container minH="100vh" maxW="container.lg" display="flex" justifyContent="center">
-			<Box as="form" onSubmit={onSubmit} w={["100%", "55%", "45%", 400]} pt={[4, 8, 12, 16]}>
+		<Container minH="100vh" maxW="container.lg" display="flex" justifyContent="center" alignItems="start">
+			<Box
+				as="form"
+				borderWidth={[0, 1]}
+				borderRadius="md"
+				onSubmit={onSubmit}
+				w={["100%", "60%", "45%", 400]}
+				p={8}
+				mt={[4, 8, 12, 16]}
+			>
 				<Input mb={2} placeholder="Roll No." value={roll} onChange={(event) => setRoll(event.target.value)} />
 				<Select mb={4} placeholder="Choose a Game" value={game} onChange={(event) => setGame(event.target.value)}>
 					{games &&
@@ -40,9 +61,10 @@ export default function GetGame() {
 							</option>
 						))}
 				</Select>
-				<Button disabled={!(roll && game)} variant="outline" type="submit">
+				<Button mb={10} disabled={!(roll && game)} variant="outline" type="submit">
 					Get Game
 				</Button>
+				{alerts}
 			</Box>
 		</Container>
 	)
