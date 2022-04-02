1947bb103c55c265d30a365fad4bd1bb37ef9a83
Atreya Choudhury <atreyachoudhury@hotmail.com>
2022-04-02 10:02:42 +0530

    copyright

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -1,10 +1,18 @@
-import { Container, Text } from "@chakra-ui/react"
+import { Container, Text, Link } from "@chakra-ui/react"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
 
 export default function Copyright() {
 	return (
 		<Container maxW="container.lg">
-			<Text py={4} fontSize="md" display="flex" justifyContent="center">
-				Created and maintained by Atreya Choudhury
+			<Text color="red.700" py={4} fontSize="md" display="flex" justifyContent="center">
+				&#169; 2022&nbsp;
+				<Link color="red.900" href="https://www.atchox.com">
+					Atreya Choudhury
+					<sup>
+						<ExternalLinkIcon ml={0.5} />
+					</sup>
+				</Link>
+				&nbsp; No Rights Reserved
 			</Text>
 		</Container>
 	)
